ab86f8b74caa0a7891e019658726baf0a31834e5
Kamil Kus <KamilKusDev@protonmail.com>
2024-05-03 21:31:36 +0200

    Initial commit

---
+++ src/test/java/kamil/vhsrental/VhsRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package kamil.vhsrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhsRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
